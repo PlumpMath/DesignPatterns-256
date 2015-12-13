143fe8dfc571c813ed227c06f4b6990a893ab8ef
Alexander Wu <alex606@att.net>
2015-12-13 06:06:44 -0600

    added bridge design pattern

---
+++ Bridge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Shape redCircle = new Circle(100, 100, 10, new RedCircle());
+            Shape greenCircle = new Circle(100, 100, 10, new GreenCircle());
+
+            greenCircle.draw();
+            redCircle.draw();
+
+            Console.ReadLine();
+        }
+    }
+}
