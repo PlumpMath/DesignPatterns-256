c13083d3c4832a384fb07df36ddc9ca5f33ec2dd
Alexander Wu <alex606@att.net>
2015-12-13 20:31:58 -0600

    added strategy pattern

---
+++ Strategy/OperationSubtract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class OperationSubtract : Strategy
+    {
+        public int doOperation(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
