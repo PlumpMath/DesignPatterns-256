15ad43e143dcb9b611ed5ffc5e0f7f034ad71d83
Alexander Wu <alex606@att.net>
2015-12-13 19:48:17 -0600

    added visitor pattern

---
+++ Visitor/ComputerPartVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public interface ComputerPartVisitor
+    {
+        void visit(Computer computer);
+        void visit(Mouse mouse);
+        void visit(Keyboard keyboard);
+        void visit(Monitor monitor);
+    }
+}
---
+++ Visitor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerPart computer = new Computer();
+            computer.accept(new ComputerPartDisplayVisitor());
+            Console.ReadLine();
+        }
+    }
+}
